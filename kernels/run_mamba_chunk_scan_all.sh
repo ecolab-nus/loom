fa4777e8c3a9d7ccc53de1bfa760a1ae29b067ef
heru wang <heru.wang@nus.edu.sg>
2026-04-28 10:09:16 +0000

    fix: fix typo in run mamba all script

--- kernels/run_mamba_chunk_scan_all.sh
+++ kernels/run_mamba_chunk_scan_all.sh
@@ -7,18 +7,28 @@ config_prefix="kernels/config_files/"
 # Run matrix entries are: "setting|config"
 run_matrix=(
   # wormhole
-  "L1024_N128_H128_G8_D128_C256|mamba_chunk_scan_wh/L1024_N128_H128_G8_D128_C256.json"
-  "L2048_N32_H128_G4_D128_C512|mamba_chunk_scan_wh/L2048_N32_H128_G4_D128_C512.json"
-  "L4096_N64_H128_G8_D128_C512|mamba_chunk_scan_wh/L4096_N64_H128_G8_D128_C512.json"
-  "L8192_N64_H128_G2_D256_C512|mamba_chunk_scan_wh/L8192_N64_H128_G2_D256_C512.json"
-  "L8192_N96_H128_G8_D256_C256|mamba_chunk_scan_wh/L8192_N96_H128_G8_D256_C256.json"
+  "L1024_N32_H128_G4_D128_C128|mamba_chunk_scan_wh/L1024_N32_H128_G4_D128_C128.json"
+  "L1024_N32_H128_G8_D128_C128|mamba_chunk_scan_wh/L1024_N32_H128_G8_D128_C128.json"
+  "L2048_N64_H128_G4_D128_C128|mamba_chunk_scan_wh/L2048_N64_H128_G4_D128_C128.json"
+  "L2048_N64_H128_G4_D128_C256|mamba_chunk_scan_wh/L2048_N64_H128_G4_D128_C256.json"
+  "L2048_N64_H128_G8_D128_C128|mamba_chunk_scan_wh/L2048_N64_H128_G8_D128_C128.json"
+  "L2048_N64_H128_G8_D128_C256|mamba_chunk_scan_wh/L2048_N64_H128_G8_D128_C256.json"
+  "L4096_N64_H128_G2_D128_C128|mamba_chunk_scan_wh/L4096_N64_H128_G2_D128_C128.json"
+  "L4096_N64_H128_G2_D128_C256|mamba_chunk_scan_wh/L4096_N64_H128_G2_D128_C256.json"
+  "L4096_N64_H128_G4_D128_C128|mamba_chunk_scan_wh/L4096_N64_H128_G4_D128_C128.json"
+  "L4096_N64_H128_G4_D128_C256|mamba_chunk_scan_wh/L4096_N64_H128_G4_D128_C256.json"
   
   # blackhole
-  "L1536_N32_H160_G4_D128_C384|mamba_chunk_scan_bh/L1536_N32_H160_G4_D128_C384.json"
+  "L1536_N32_H160_G4_D128_C192|mamba_chunk_scan_bh/L1536_N32_H160_G4_D128_C192.json"
+  "L1536_N32_H160_G8_D128_C192|mamba_chunk_scan_bh/L1536_N32_H160_G8_D128_C192.json"
+  "L3072_N64_H160_G4_D128_C192|mamba_chunk_scan_bh/L3072_N64_H160_G4_D128_C192.json"
+  "L3072_N64_H160_G4_D128_C384|mamba_chunk_scan_bh/L3072_N64_H160_G4_D128_C384.json"
+  "L3072_N64_H160_G8_D128_C192|mamba_chunk_scan_bh/L3072_N64_H160_G8_D128_C192.json"
   "L3072_N64_H160_G8_D128_C384|mamba_chunk_scan_bh/L3072_N64_H160_G8_D128_C384.json"
-  "L6144_N48_H160_G4_D128_C768|mamba_chunk_scan_bh/L6144_N48_H160_G4_D128_C768.json"
-  "L12288_N64_H160_G2_D256_C768|mamba_chunk_scan_bh/L12288_N64_H160_G2_D256_C768.json"
-  "L12288_N64_H160_G8_D256_C384|mamba_chunk_scan_bh/L12288_N64_H160_G8_D256_C384.json"
+  "L6144_N64_H160_G2_D128_C192|mamba_chunk_scan_bh/L6144_N64_H160_G2_D128_C192.json"
+  "L6144_N64_H160_G2_D128_C384|mamba_chunk_scan_bh/L6144_N64_H160_G2_D128_C384.json"
+  "L6144_N64_H160_G4_D128_C192|mamba_chunk_scan_bh/L6144_N64_H160_G4_D128_C192.json"
+  "L6144_N64_H160_G4_D128_C384|mamba_chunk_scan_bh/L6144_N64_H160_G4_D128_C384.json"
 
 )
 
