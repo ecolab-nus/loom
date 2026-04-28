fd7dcf19477a3cb829a38ac217cd07ad9f8ea6f5
heru wang <heru.wang@nus.edu.sg>
2026-04-28 12:25:12 +0000

    feat: update mqa size

--- kernels/run_mqa_decode_all.sh
+++ kernels/run_mqa_decode_all.sh
@@ -7,63 +7,24 @@ config_prefix="kernels/config_files/mqa_decode/"
 hw_spec_prefix="third_party/loom-mlar/tests/2d_mesh/"
 
 # Run matrix entries are: "kernel_size|config|hw_spec"
-# wh: hw spec with x<=8 and y<=8
+# wh
 run_matrix_wh=(
-  "B2_H32_L512_D64|fixed_block_size.json|2d_mesh_torus_x2y2.mlir"
-  "B2_H32_L512_D128|fixed_block_size.json|2d_mesh_torus_x2y2.mlir"
-  "B2_H32_L1024_D64|fixed_block_size.json|2d_mesh_torus_x2y2.mlir"
-  "B2_H32_L1024_D128|fixed_block_size.json|2d_mesh_torus_x2y2.mlir"
-  "B2_H32_L2048_D64|fixed_block_size.json|2d_mesh_torus_x4y2.mlir"
-  "B2_H32_L2048_D128|fixed_block_size.json|2d_mesh_torus_x4y2.mlir"
-  "B2_H32_L4096_D64|fixed_block_size.json|2d_mesh_torus_x8y2.mlir"
-  "B2_H32_L4096_D128|fixed_block_size.json|2d_mesh_torus_x8y2.mlir"
-  "B4_H32_L512_D64|fixed_block_size.json|2d_mesh_torus_x2y4.mlir"
-  "B4_H32_L512_D128|fixed_block_size.json|2d_mesh_torus_x2y4.mlir"
-  "B4_H32_L1024_D64|fixed_block_size.json|2d_mesh_torus_x2y4.mlir"
-  "B4_H32_L1024_D128|fixed_block_size.json|2d_mesh_torus_x2y4.mlir"
-  "B4_H32_L2048_D64|fixed_block_size.json|2d_mesh_torus_x4y4.mlir"
-  "B4_H32_L2048_D128|fixed_block_size.json|2d_mesh_torus_x4y4.mlir"
-  "B4_H32_L4096_D64|fixed_block_size.json|2d_mesh_torus_x8y4.mlir"
-  "B4_H32_L4096_D128|fixed_block_size.json|2d_mesh_torus_x8y4.mlir"
-  "B8_H32_L512_D64|fixed_block_size.json|2d_mesh_torus_x2y8.mlir"
-  "B8_H32_L512_D128|fixed_block_size.json|2d_mesh_torus_x2y8.mlir"
-  "B8_H32_L1024_D64|fixed_block_size.json|2d_mesh_torus_x2y8.mlir"
-  "B8_H32_L1024_D128|fixed_block_size.json|2d_mesh_torus_x2y8.mlir"
-  "B8_H32_L2048_D64|fixed_block_size.json|2d_mesh_torus_x4y8.mlir"
-  "B8_H32_L2048_D128|fixed_block_size.json|2d_mesh_torus_x4y8.mlir"
-  "B8_H32_L4096_D64|fixed_block_size.json|2d_mesh_torus_x8y8.mlir"
-  "B8_H32_L4096_D128|fixed_block_size.json|2d_mesh_torus_x8y8.mlir"
+  "B8_H32_L16384_D64|fixed_block_size.json|2d_mesh_torus_x8y8.mlir"
+  "B8_H32_L16384_D128|fixed_block_size.json|2d_mesh_torus_x8y8.mlir"
+  "B16_H32_L8192_D64|fixed_block_size.json|2d_mesh_torus_x8y8.mlir"
+  "B16_H32_L8192_D128|fixed_block_size.json|2d_mesh_torus_x8y8.mlir"
+  "B32_H32_L4096_D64|fixed_block_size.json|2d_mesh_torus_x8y8.mlir"
+  "B32_H32_L4096_D128|fixed_block_size.json|2d_mesh_torus_x8y8.mlir"
 )
 
-# bh: hw spec with x>8 or y>8
+# bh
 run_matrix_bh=(
-  "B2_H32_L5120_D64|fixed_block_size.json|2d_mesh_torus_x10y2.mlir"
-  "B2_H32_L5120_D128|fixed_block_size.json|2d_mesh_torus_x10y2.mlir"
-  "B2_H32_L6144_D64|fixed_block_size.json|2d_mesh_torus_x12y2.mlir"
-  "B2_H32_L6144_D128|fixed_block_size.json|2d_mesh_torus_x12y2.mlir"
-
-  "B4_H32_L5120_D64|fixed_block_size.json|2d_mesh_torus_x10y4.mlir"
-  "B4_H32_L5120_D128|fixed_block_size.json|2d_mesh_torus_x10y4.mlir"
-  "B4_H32_L6144_D64|fixed_block_size.json|2d_mesh_torus_x12y4.mlir"
-  "B4_H32_L6144_D128|fixed_block_size.json|2d_mesh_torus_x12y4.mlir"
-
-  "B8_H32_L5120_D64|fixed_block_size.json|2d_mesh_torus_x10y8.mlir"
-  "B8_H32_L5120_D128|fixed_block_size.json|2d_mesh_torus_x10y8.mlir"
-  "B8_H32_L6144_D64|fixed_block_size.json|2d_mesh_torus_x12y8.mlir"
-  "B8_H32_L6144_D128|fixed_block_size.json|2d_mesh_torus_x12y8.mlir"
-
-  "B10_H32_L512_D64|fixed_block_size.json|2d_mesh_torus_x2y10.mlir"
-  "B10_H32_L512_D128|fixed_block_size.json|2d_mesh_torus_x2y10.mlir"
-  "B10_H32_L1024_D64|fixed_block_size.json|2d_mesh_torus_x2y10.mlir"
-  "B10_H32_L1024_D128|fixed_block_size.json|2d_mesh_torus_x2y10.mlir"
-  "B10_H32_L2048_D64|fixed_block_size.json|2d_mesh_torus_x4y10.mlir"
-  "B10_H32_L2048_D128|fixed_block_size.json|2d_mesh_torus_x4y10.mlir"
-  "B10_H32_L4096_D64|fixed_block_size.json|2d_mesh_torus_x8y10.mlir"
-  "B10_H32_L4096_D128|fixed_block_size.json|2d_mesh_torus_x8y10.mlir"
-  "B10_H32_L5120_D64|fixed_block_size.json|2d_mesh_torus_x10y10.mlir"
-  "B10_H32_L5120_D128|fixed_block_size.json|2d_mesh_torus_x10y10.mlir"
-  "B10_H32_L6144_D64|fixed_block_size.json|2d_mesh_torus_x12y10.mlir"
-  "B10_H32_L6144_D128|fixed_block_size.json|2d_mesh_torus_x12y10.mlir"
+  "B10_H32_L24576_D64|fixed_block_size.json|2d_mesh_torus_x12y10.mlir"
+  "B10_H32_L24576_D128|fixed_block_size.json|2d_mesh_torus_x12y10.mlir"
+  "B20_H32_L12288_D64|fixed_block_size.json|2d_mesh_torus_x12y10.mlir"
+  "B20_H32_L12288_D128|fixed_block_size.json|2d_mesh_torus_x12y10.mlir"
+  "B40_H32_L6144_D64|fixed_block_size.json|2d_mesh_torus_x12y10.mlir"
+  "B40_H32_L6144_D128|fixed_block_size.json|2d_mesh_torus_x12y10.mlir"
 )
 
 for run_item in "${run_matrix_wh[@]}"; do
