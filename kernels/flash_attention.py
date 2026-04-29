fix: explicit broadcast flash_attention

kernels/flash_attention.py
@@ -15,6 +15,7 @@ import helion
 import helion.language as hl
 
 from loom import LoomKernel
+from helion_mlir.custom_op import broadcast
 
 
 def _flash__attention(
@@ -33,27 +34,29 @@ def _flash__attention(
     out_ = torch.empty_like(q_view)
     sm_scale = 1.0 / math.sqrt(head_dim)
     for tile_b, tile_m in hl.tile([q_view.size(0), m_dim]):
-        qk_scale_dev = hl.full([], sm_scale * 1.44269504, dtype=torch.float16)
-        m_i = hl.full([tile_b, tile_m], float("-inf"), dtype=torch.float16)
+        qk_scale_dev = hl.full([], sm_scale, dtype=torch.float16)
+        m_i = hl.full([tile_b, tile_m, 1], float("-inf"), dtype=torch.float16)
         l_i = torch.full_like(m_i, 1.0)
         acc = hl.zeros([tile_b, tile_m, head_dim], dtype=torch.float16)
         q = q_view[tile_b, tile_m, :]
         for tile_n in hl.tile(v_view.size(1)):
             k = k_view[tile_b, :, tile_n]
             qk = torch.bmm(q, k)
-            m_ij = torch.maximum(m_i, torch.amax(qk, -1) * qk_scale_dev)
-            qk = qk * qk_scale_dev - m_ij[:, :, None]
-            p = torch.exp2(qk)
-            l_ij = torch.sum(p, -1)
-            alpha = torch.exp2(m_i - m_ij)
+            m_ij = torch.maximum(m_i, torch.amax(qk, -1, keepdim=True) * qk_scale_dev)
+            m_ij_broad = broadcast(m_ij, 2, [m_ij.size(0), m_ij.size(1), tile_n])
+            qk = qk * qk_scale_dev - m_ij_broad
+            p = torch.exp(qk)
+            l_ij = torch.sum(p, -1, keepdim=True)
+            alpha = torch.exp(m_i - m_ij)
             l_i = l_i * alpha + l_ij
-            acc = acc * alpha[:, :, None]
+            acc = acc * alpha
             v = v_view[tile_b, tile_n, :]
             p = p.to(v.dtype)
             acc = torch.baddbmm(acc, p, v)
             m_i = m_ij
-        m_i += torch.log2(l_i)
-        acc = acc / l_i[:, :, None]
+        m_i += torch.log(l_i)
+        l_i_broadcast = broadcast(l_i, 2, [l_i.size(0), l_i.size(1), head_dim])
+        acc = acc / l_i_broadcast
         out_[tile_b, tile_m, :] = acc.to(out_.dtype)
     return out_.view(q_in.size())
 
@@ -65,6 +68,8 @@ class FlashAttention(LoomKernel):
     B: int = 256
     L: int = 4096
     d: int = 128
+
+    assume_divisible_tiles = True
 
     # Assign the helion-decorated function as a class attribute.
     # We cannot stack @staticmethod with @helion.kernel because the helion